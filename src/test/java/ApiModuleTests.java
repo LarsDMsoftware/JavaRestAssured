8c10c7a929c177efd9cfb94a3eb0963bf2334267
Lars De Maere <lars.demaere@bignited.be>
2022-06-02 10:56:25 +0200

    Switching project

--- src/test/java/ApiModuleTests.java
+++ src/test/java/ApiModuleTests.java
@@ -19,23 +19,32 @@ public class ApiModuleTests {
     public void PostTestingApi() throws IOException, ParseException {
         // https://petstore3.swagger.io/api/v3/pet
         RequestSpecification request = RestAssured.given();
+        RequestSpecification requestPut = RestAssured.given();
 
         JSONParser parser = new JSONParser();
-        JSONObject a = (JSONObject) parser.parse(new FileReader("src/test/java/pet.json"));
+        JSONObject pet = (JSONObject) parser.parse(new FileReader("src/test/java/pet.json"));
+        JSONObject updatedPet = (JSONObject) parser.parse(new FileReader("src/test/java/pet.json"));
 
         request.header("Content-Type", "application/json"); // Add the Json to the body of the request
-        request.body(a); // Post the request and check the response
+        request.body(pet); // Post the request and check the response
 
+        System.out.println(pet.toJSONString());
+        System.out.println(pet.toString());
         Response response = request.post("https://petstore3.swagger.io/api/v3/pet/");
         ResponseBody body = response.getBody();
 
-        System.out.println(body.asString());
+        System.out.println("Body of response " + body.asString());
+        //.then().body("id", equalTo("10"));
+
+        //updatedPet
+
+        System.out.println(body["id"]);
         System.out.println("The status received: " + response.statusLine());
         System.out.println("Trying get request");
 
         get("https://petstore3.swagger.io/api/v3/pet/10").then().body("id", equalTo(10));
 
-        given().param(a.toJSONString()).
+        given().param(pet.toJSONString()).
                 when().
                 post("https://petstore3.swagger.io/api/v3/pet/").
                 then().
@@ -43,6 +52,14 @@ public class ApiModuleTests {
 
         delete("https://petstore3.swagger.io/api/v3/pet/10");
         get("https://petstore3.swagger.io/api/v3/pet/10").then().body("id", equalTo(10));
+
+        requestPut.header("Content-Type", "application/json"); // Add the Json to the body of the request
+        requestPut.body(updatedPet); // Post the request and check the response
+
+        Response responsePut = requestPut.put("https://petstore3.swagger.io/api/v3/pet/10/");
+        ResponseBody putBody = response.getBody();
+
+        System.out.println(putBody.asString());
 
 
     }
